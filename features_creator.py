d219453c826465151ae7a0d87412c1325f71dada
apokrif333 <41585594+apokrif333@users.noreply.github.com>
2019-05-17 10:28:37 -0400

    Work

--- features_creator.py
+++ features_creator.py
@@ -6,9 +6,8 @@ import pandas as pd; pd.options.display.max_rows = 20_000
 
 PATH_TO_SAVE = 'database/'
 
-if __name__ == "__main__":
-    df = pd.read_excel('database/DJI Features.xlsx', converters={'Date': pd.to_datetime})
 
+def create_y(df: pd.DataFrame):
     df['y_month'] = [0] * len(df)
     for day in range(0, len(df.Date)):
         print(df.Date[day])
@@ -19,5 +18,25 @@ if __name__ == "__main__":
         else:
             print(closes_next_month)
             df.loc[day, 'y_month'] = None
+
+    tl.save_csv(PATH_TO_SAVE, 'DJI Features_1', df)
+
+
+if __name__ == "__main__":
+
+    df = pd.read_csv('database/DJI Features_1.csv', converters={'Date': pd.to_datetime})
+
+    # Create momentums
+    for i in range(1, 25):
+        df[f"Momentum_perc_{i}"] = [0] * len(df)
+
+    for day in range(0, len(df.Date)):
+        print(df.Date[day])
+        for i in range(1, 25):
+            closes_next_month = df[df.Date[day] - rd(months=i) >= df.Date]['Close']
+            if len(closes_next_month) == 0:
+                continue
+
+            df.loc[day, f"Momentum_perc_{i}"] = (closes_next_month.iloc[0] / df.Close[day] - 1) * 100
 
     tl.save_csv(PATH_TO_SAVE, 'DJI Features_1', df)
