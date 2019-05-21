44c6fe505dea263dbb0b4f92805c40142cdddb3a
apokrif333 <41585594+apokrif333@users.noreply.github.com>
2019-05-21 10:43:44 -0400

    Work

--- features_creator.py
+++ features_creator.py
@@ -1,4 +1,5 @@
 from dateutil.relativedelta import relativedelta as rd
+import datetime as dt
 from libs import trading_lib as tl
 
 import pandas as pd; pd.options.display.max_rows = 20_000
@@ -48,10 +49,7 @@ def create_sma(df: pd.DataFrame):
     tl.save_csv(PATH_TO_SAVE, 'DJI Features_2', df)
 
 
-if __name__ == "__main__":
-
-    df = pd.read_csv('database/DJI Features_2.csv', converters={'Date': pd.to_datetime})
-
+def create_vol(df: pd.DataFrame):
     vol_columns = [.5, .75] + list(range(1, 13))
     for i in vol_columns:
         df[f"Annual_vol_perc_{i}"] = [0] * len(df)
@@ -78,3 +76,58 @@ if __name__ == "__main__":
     tl.save_csv(PATH_TO_SAVE, 'DJI Features_3', df)
 
 
+def mom_and_sma_one_hot_encoding(df: pd.DataFrame):
+    for i in range(1, 25):
+        df[f"Momentum_bool_{i}"] = np.array(df[f"Momentum_perc_{i}"] >= 0).astype('int')
+
+    sma_columns = [10, 20] + list(range(25, 425, 25))
+    for number in sma_columns:
+        df[f"SMA_bool_{number}"] = np.array(df[f"SMA_perc_{number}"] >= 0).astype('int')
+
+    tl.save_csv(PATH_TO_SAVE, 'DJI Features_4', df)
+
+
+def add_time_as_feature(df: pd.DataFrame):
+    # Delete sunday and saturday
+    week_days = df.Date.dt.weekday
+    df.drop((week_days[week_days == 6]).index, inplace=True)
+
+    week_days = df.Date[df.Date >= dt.datetime(1953, 1, 1)].dt.weekday
+    df.drop((week_days[week_days == 5]).index, inplace=True)
+
+    # Create one-hot columns with time
+    uniq_months = sorted(df.Date.dt.month.unique())
+    uniq_week_days = sorted(df.Date.dt.weekday.unique())
+    uniq_days = sorted(df.Date.dt.day.unique())
+
+    for month in uniq_months:
+        df[f"Month_{month}"] = (df.Date.dt.month == month).astype('int')
+    for week_d in uniq_week_days:
+        df[f"Week_d_{week_d}"] = (df.Date.dt.weekday == week_d).astype('int')
+    for day in uniq_days:
+        df[f"Day_{day}"] = (df.Date.dt.day == day).astype('int')
+
+    tl.save_csv(PATH_TO_SAVE, 'DJI Features_5', df)
+
+
+def cut_data(df: pd.DataFrame):
+    start_index = df[df['Momentum_perc_24'] != 0].index[0]
+    end_index = df['y_month'][np.isnan(df['y_month'])].index[0]
+
+    tl.save_csv(PATH_TO_SAVE, 'DJI Features_6', df[start_index:end_index])
+
+
+def check_data_wholeness(df: pd.DataFrame):
+    calc_dates = np.ediff1d(np.array(df.Date))
+    dates_diff = (calc_dates / 86_400 / 1_000_000_000).astype('int')
+    dates_diff = np.insert(dates_diff, 0, 0)
+    df['Days_jump'] = dates_diff
+    print(df[df['Days_jump'] > 4])
+
+
+if __name__ == "__main__":
+    # !We have saturdays from start to 1952 year!
+    # !We have empty data from 7/30/1914 to 12/12/1914!
+
+    df = pd.read_csv('database/DJI Features_6.csv', converters={'Date': pd.to_datetime})
+    check_data_wholeness(df)
