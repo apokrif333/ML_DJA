3c2a4a2157e6446ba7b8852d247fdd92fd576047
apokrif333 <41585594+apokrif333@users.noreply.github.com>
2019-05-31 08:45:35 -0400

    Work

--- Classification (bootstrap).py
+++ Classification (bootstrap).py
@@ -29,29 +29,22 @@ def features_pipeline(df) -> np.array:
 
 def kNN():
     model = KNeighborsClassifier(n_neighbors=3, n_jobs=-1)
-    # start = 24
-    # for i in range(start, np_X_train.shape[1]):
-    #     X = np_X_train[:, start:i+1]
-    #     scores = cross_val_score(model, X, y_train, cv=5, n_jobs=-1)
-    #     print(f"Last feature name {features_names[i-1]}. "
-    #           f"Features_quantity {len(features_names[start:i+1])}. "
-    #           f"Score: {scores.mean()}")
+    start = 0
+    for i in range(start, np_X_train.shape[1]):
+        X = np_X_train[:, start:i+1]
+        scores = cross_val_score(model, X, y_train, cv=5, n_jobs=-1)
+        print(f"Last feature name {features_names[i]}. "
+              f"Features_quantity {len(features_names[start:i+1])}. "
+              f"Score: {scores.mean()}")
 
     model.fit(np_X_train, y_train)
-
-    df_test = pd.read_csv('teach_data/X_test.csv')
-    y = df_test['y_month']
-    df_test.drop(axis=1, columns=['Date', 'y_month', 'Close'], inplace=True)
-    np_X_test = features_pipeline(df_test)
-
-    y_predict = model.predict(np_X_test)
-    print(accuracy_score(y, y_predict))
+    y_predict = model.predict(np_X_valid)
+    print(accuracy_score(y_valid, y_predict))
 
 
-X_df = pd.read_csv('database/DJI Features_8.csv', converters={'Date': pd.to_datetime})
+X_df = pd.read_csv('teach_data/bootstrap/X_train.csv', converters={'Date': pd.to_datetime})
 y = X_df['y_month']
 X_df.drop(axis=1, columns=['y_month', 'Close'], inplace=True)
-
 
 X_train, X_valid, y_train, y_valid = train_test_split(X_df, y, test_size=0.15, random_state=17)
 train_dates = X_train['Date']
@@ -63,9 +56,8 @@ features_names = X_train.columns
 np_X_train = features_pipeline(X_train)
 np_X_valid = features_pipeline(X_valid)
 
-# model = RandomForestClassifier(n_estimators=1_000, max_depth=10,random_state=17, n_jobs=-1)
 kNN()
-input()
 
-feat_importance = dict(zip(features_names, model.feature_importances_))
-print(sorted(feat_importance.items(), key=lambda x: x[1], reverse=True))
+# model = RandomForestClassifier(n_estimators=1_000, max_depth=10,random_state=17, n_jobs=-1)
+# feat_importance = dict(zip(features_names, model.feature_importances_))
+# print(sorted(feat_importance.items(), key=lambda x: x[1], reverse=True))
